4101e17e66e5882e1feb8c58f6400d4f786a9146
Oksana <oksana_sorokoletova1459@mail.ru>
2023-03-01 17:08:40 -0800

    create cycle

---
+++ cycle for.py
@@ -0,0 +1,5 @@
+number=int(input("Number: "))
+sum= int(str(number)[0])+int(str(number)[1])+int(str(number)[2])
+print("Sum= ", sum)
+umnog= int(str(number)[0])*int(str(number)[1])*int(str(number)[2])
+print("Umnogenie= ", umnog)
