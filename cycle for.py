ef61ea5fd4df4374b4c8f43813f23373b8fde9e0
Oksana <oksana_sorokoletova1459@mail.ru>
2023-03-15 16:08:16 -0700

    create task 1

--- cycle for.py
+++ cycle for.py
@@ -3,3 +3,10 @@ sum= int(str(number)[0])+int(str(number)[1])+int(str(number)[2])
 print("Sum= ", sum)
 umnog= int(str(number)[0])*int(str(number)[1])*int(str(number)[2])
 print("Umnogenie= ", umnog)
+
+num = int(input("Введите целое: "))
+sum = 0
+while (num != 0):
+    sum = sum + num % 10
+    num = num // 10
+print("Сумма цифр числа равна: ", sum)
